bd135cc36e3795af1792c06ceb3b11cb6e64e65f
bigfudge83 <bigfudge83@gmail.com>
2014-08-28 15:14:42 +0930

    Add files
    files tute

---
+++ driver.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+
+var x;
+
+document.getElementById("demo").innerHTML = "Do you want to play SuDoKu?";
---
+++ functions.js
@@ -0,0 +1,12 @@
+//make sudoku generator
+
+function global() {
+
+};
+
+function firstNo () {
+	var x = Math.floor((Math.random() * 9) + 1);
+	return x;
+};
+
+function 
